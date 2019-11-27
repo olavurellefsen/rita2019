f785d4038db88becf973355fc60416971bc81c02
olavurellefsen <oe@tokni.com>
2019-11-27 22:58:37 +0000

    Shorter menu items

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,9 +8,9 @@ export class Sidebar extends Component {
     this.state = {
       tabs: [
         { content: 'Heim', href: 'intro' },
-        { content: 'Hvat og hvussu', href: 'one' },
+        { content: 'Hvat', href: 'one' },
         { content: 'Um #rita1tíma', href: 'two' },
-        { content: 'Skráset flokkin', href: 'three' },
+        { content: 'Skráset', href: 'three' },
       ],
     };
   }
